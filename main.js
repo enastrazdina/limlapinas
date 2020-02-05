f84ad2cfe7eb2cb9546d6a292d1f0c19bf7affb3
enastrazdina <strazdina.ena@gmail.com>
2020-02-05 11:22:33 +0200

    removed comments and console.log()

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-    // var postIt = $('.sticker');
     var originalEl = $('.original');
-
-    // $(postIt).draggable();
-
 
     var deleteSticker = function() {
 
-        console.log(this);
         $(this).parent().remove();
     }
 
@@ -29,46 +24,9 @@ $(document).ready(function() {
         });
     }
 
-
     cloneSticker();
 
     $('.new').on('click', cloneSticker);
-
     $('.add').on('click', moveSticker);
 
 });
-
-
-// newSticker.removeClass('original').addClass('sticker').appendTo('body').draggable();
-
-
-
-
-
-
-// newSticker.removeClass('original').addClass('sticker');
-// newSticker.draggable();
-
-
-
-
-// $('.close').on('click', funClose);
-/*
-
-$('.original')
-    .clone()
-    .removeClass('original')
-    .addClass('.sticker')
-    .appendTo('body')
-    .draggable()
-var bodyWidth = document.body.clientWidth
-var bodyHeight = document.body.clientHeight;
-var randPosX = Math.floor((Math.random() * bodyWidth));
-var randPosY = Math.floor((Math.random() * bodyHeight));
-
-$('.original').css({
-    'left': randPosX,
-    'top': randPosY
-});
-
-*/
