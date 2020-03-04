96ce77745b9c0e274f534eb505c1f2aa09ca03a2
enastrazdina <strazdina.ena@gmail.com>
2020-03-04 16:08:52 +0200

    config object

---
+++ config.js
@@ -0,0 +1,13 @@
+
+var config = {
+    defaults: {
+        theme: 'light'
+    },
+    getConfig: function() {
+        return {defaults};
+    }, 
+    setConfig: function() {
+
+    },
+};
+console.log(config.getConfig());
