67ea0e3433de3957f10c5198a7319828af135d86
enastrazdina <strazdina.ena@gmail.com>
2020-03-10 14:57:30 +0200

    localStorage error

--- config.js
+++ config.js
@@ -3,25 +3,11 @@ var config = {
     theme: 'light',
     lang: 'en'
   },
-  getTheme: function () {
+  get: function () {
     return this.defaults;
   },
-  setTheme(key, value) {
-    var newUserConfig = Object.assign({}, {}, {
-      key: value
-    });
-    console.log(newUserConfig);
+  set: function (key, value) {
+    var newUserConfig = Object.assign({}, {}, {key: value});
     localStorage.setItem('config', JSON.stringify(newUserConfig));
-    console.log(newUserConfig);
   },
-
-  getLang: function () {
-    return this.defaults;
-  },
-  setLang(key, value) {
-    var newUserLangConfig = Object.assign({}, {}, {
-      key: value
-    });
-    localStorage.setItem('langConfig', JSON.stringify(newUserLangConfig));
-  }
 };
