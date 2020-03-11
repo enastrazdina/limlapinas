4c806fba8f0edaa690b8409e551a9ac6d9e7a549
enastrazdina <strazdina.ena@gmail.com>
2020-03-11 15:00:53 +0200

    localStorage

--- config.js
+++ config.js
@@ -7,13 +7,10 @@ var config = {
     return this.defaults;
   },
   set: function (key, value) {
-    var newUserConfig = Object.assign({}, {}, {
-      [key]: value
-    });
-    localStorage.setItem('config', JSON.stringify(newUserConfig));
     var currentData = localStorage.getItem('config');
     var dataStringToObject = JSON.parse(currentData);
     var newDataObject = Object.assign({}, dataStringToObject, {[key]: value});
+    localStorage.setItem('config', JSON.stringify(newDataObject));
     console.log(newDataObject);
   },
 
