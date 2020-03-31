a50dd7c4f0bb6afd68d7259b2a3179aa8dd3a9f0
enastrazdina <strazdina.ena@gmail.com>
2020-03-31 16:39:57 +0300

    managing stickers in db in progress

---
+++ stickers.js
@@ -0,0 +1,17 @@
+var stickers = [{
+  update: function (id, data) {
+
+  },
+  create: function (sticker) {
+
+  },
+  delete: function (id) {
+
+  },
+  getAll: function () {
+
+  },
+  reorder: function (id) {
+
+  },
+}];
