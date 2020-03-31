341e0aab74ccffb5526b36bf3c84f43af54587fc
enastrazdina <strazdina.ena@gmail.com>
2020-03-31 16:38:44 +0300

    added function for uuid for id!

--- main.js
+++ main.js
@@ -105,3 +105,20 @@ $(document).ready(function () {
   onAppLoad();
   cloneSticker();
 });
+
+function uuid() {
+  var seed = Date.now();
+  if (window.performance && typeof window.performance.now === "function") {
+    seed += performance.now();
+  }
+
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (seed + Math.random() * 16) % 16 | 0;
+    seed = Math.floor(seed / 16);
+
+    return (c === 'x' ? r : r & (0x3 | 0x8)).toString(16);
+  });
+
+  return uuid;
+}
+var value = uuid();
