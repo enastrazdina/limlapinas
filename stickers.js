7ed5a2f4c3a7d12c800b42900e8ad5878a7de323
enastrazdina <strazdina.ena@gmail.com>
2020-05-12 21:28:29 +0300

    create function. think I complicated it and something is not working

--- stickers.js
+++ stickers.js
@@ -3,14 +3,19 @@ var stickers = [{
 
   },
   create: function (sticker) {
+    var allStickers = Object.values(this.getAll());
+    allStickers.push({
+      sticker
+    });
 
+    localStorage.setItem('stickers', JSON.stringify(allStickers));
   },
   delete: function (id) {
 
   },
   getAll: function () {
-    var stickers = localStorage.getItem('stickers');
-    return stickers;
+    var stickers = localStorage.getItem('stickers') || '[]';
+    return JSON.parse(stickers);
   },
   reorder: function (id) {
 
